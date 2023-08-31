8044c9eec04ae2cc7ca36b76bf8d9304448557dc
roze <wonder_0128@naver.com>
2023-08-31 17:16:25 +0900

    20230831-2

--- scrolltrigger-js_base/src/script.js
+++ scrolltrigger-js_base/src/script.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
-    var trigger = new ScrollTrigger();
+    var trigger = new ScrollTrigger({
+        offset:{
+            x : 0,
+            y : 250
+        }
+    });
 });
